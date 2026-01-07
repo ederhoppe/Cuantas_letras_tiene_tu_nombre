7b35b88371add0cbcfdc39d43d112a518ca84c40
ederhoppe <eder.creaper@gmail.com>
2026-01-07 17:29:59 -0600

    Add files via upload

---
+++ Ejercicio_1_T1.py
@@ -0,0 +1,6 @@
+# 1. Escribe un programa que le pida su nombre al usuario 
+# y como salida le diga cuantos caracteres tiene su nombre.
+
+print('Bienvenido, ¿Quieres conocer cuantas letras tiene tu nombre?')
+nombre = input('Averigualo: ')
+print(len(nombre))
